cae1e01860f491a87f20b4c4745362b2557c6636
cb5824 <craig.baylis@gmail.com>
2018-03-16 02:01:52 +0000

    added tests

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -3,6 +3,22 @@ require 'rails_helper'
 
 feature 'user sees the landing page' do
 
+  let!(:recipe) do
+    FactoryBot.create(:recipe)
+  end
+
+  let!(:recipe2) do
+    FactoryBot.create(:recipe)
+  end
+
+  let!(:user) do
+    FactoryBot.create(:user)
+  end
+
+  let!(:user2) do
+    FactoryBot.create(:user)
+  end
+
   scenario "user navigates to the landing page" do
     visit root_path
 
@@ -10,5 +26,29 @@ feature 'user sees the landing page' do
     expect(page).to have_link('List of all recipes')
     expect(page).to have_link('Previous List')
 
+  end
+
+  scenario "An unauthenticated user sees My Account" do
+    sign_in_user
+    visit root_path
+    expect(page).to have_link('My Account')
+  end
+
+  scenario "An unauthenticated user does not see My Account" do
+    visit root_path
+    expect(page).to_not have_link('My Account')
+  end
+
+  scenario "List dropdown is hidden by default" do
+    visit root_path
+
+    expect(page).to have_selector :css, 'div#list_overlay.hide'
+
+  end
+
+  scenario "Top recipes are displayed" do
+    visit root_path
+    expect(page).to have_content(recipe.name)
+    expect(page).to have_content(recipe2.name)
   end
 end
