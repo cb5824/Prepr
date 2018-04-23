d2700e6b141c97a5b40d408b4bb6d56f15d83a55
cb5824 <craig.baylis@gmail.com>
2018-04-23 02:40:01 +0000

    added infinite scrolling to main page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
     ordered_recipes = Recipe.includes(:favorites).sort_by {|recipe| recipe.created_at}
     @recent_recipes = ordered_recipes[0..3]
     @top_recipes = (ordered_recipes.sort_by {|recipe| recipe.favorites.length}).reverse.shift(5)
+    @recipes = Recipe.paginate(:page => params[:page], :per_page => 5)
     if user_signed_in? && current_user.lists[0].nil?
       @current_list = List.create(user_id: current_user.id)
     end
